5b251d1edfa98ee6944f588194654a6969dbc914
russelhealey <rhealey@oldworldind.com>
2012-12-09 12:53:20 -0600

    test conflict from mac

--- test2/src/main/java/test/TestGit.java
+++ test2/src/main/java/test/TestGit.java
@@ -2,6 +2,6 @@ package test;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test create conflict");
     }
 }
