ec179b3542cfb8e5deb1f9d91264c18fe4e08681
russelhealey <rhealey@oldworldind.com>
2012-12-10 14:58:24 -0600

    TestCommit

--- test2/src/main/java/test/TestGit.java
+++ test2/src/main/java/test/TestGit.java
@@ -2,6 +2,6 @@ package test;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("test create conflict");
+        System.out.println("test push");
     }
 }
