693b2932c7fbf11b095329e5db036c83cd61b6ac
healey <healey@rhealeyxpvb4.oldworldind.com>
2012-12-11 13:29:19 -0600

    test conflict

--- test2/src/main/java/test/TestGit.java
+++ test2/src/main/java/test/TestGit.java
@@ -3,6 +3,5 @@ package test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Git is great ok");
-        System.out.println("Git is great");
     }
 }
