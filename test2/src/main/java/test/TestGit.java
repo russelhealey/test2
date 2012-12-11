9d32fac250f838cc2365fa9ce1b3b5c781079722
healey <healey@rhealeyxpvb4.oldworldind.com>
2012-12-11 13:14:03 -0600

    test commit

--- test2/src/main/java/test/TestGit.java
+++ test2/src/main/java/test/TestGit.java
@@ -2,6 +2,6 @@ package test;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("test push again and again");
+        System.out.println("Git is great");
     }
 }
