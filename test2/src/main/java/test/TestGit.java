817e7d54a5adb0e6c939c878bbb11f3beb2a7ee6
healey <healey@rhealeyxpvb4.oldworldind.com>
2012-12-14 10:35:50 -0600

    test4

--- test2/src/main/java/test/TestGit.java
+++ test2/src/main/java/test/TestGit.java
@@ -3,7 +3,7 @@ package test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Git is great ok");
-        // ok
+        // ok 2
         // create conflict
         // change
     }
