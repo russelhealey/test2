d8e1a017090181af5f0ab8d0368e856ac1731754
russelhealey <rhealey@oldworldind.com>
2012-12-14 14:26:42 -0600

    test3frommac

--- test2/src/main/java/test/TestGit.java
+++ test2/src/main/java/test/TestGit.java
@@ -3,7 +3,7 @@ package test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Git is great ok");
-        // ok 2
+        // ok 2 create second conflict
         // create conflict
         // change
     }
