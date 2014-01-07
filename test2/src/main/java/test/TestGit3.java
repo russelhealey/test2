a353295735f6cbdb03a933f85ab471d3c5178cd8
russelhealey <rhealey@oldworldind.com>
2014-01-07 11:54:43 -0600

    Test Conflict

--- test2/src/main/java/test/TestGit3.java
+++ test2/src/main/java/test/TestGit3.java
@@ -1,1 +1,10 @@
-Test this file
+package test;
+
+public class TestGit3 {
+    public static void main(String[] args) {
+        System.out.println("Git is great ok");
+        // ok 2 create second conflict
+        // create conflict
+        // change
+    }
+}
