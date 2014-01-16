0d36ca7b8d3d09dc28ec35eef1c60910ef0a6fa3
russelhealey <rhealey@oldworldind.com>
2014-01-16 15:04:28 -0600

    test trying to merge on mac

--- test2/src/main/java/test/TestGit.java
+++ test2/src/main/java/test/TestGit.java
@@ -3,7 +3,6 @@ package test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Git windows");
-        //test2
         //test
         //test dif change
     }
